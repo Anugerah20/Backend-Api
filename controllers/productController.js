1c045bda9a36b78c2af3f685e2eaa3829aced02b
Nabil Anugerah <nabilpangestu123@gmail.com>
2023-10-16 21:01:35 +0700

    create controllers

---
+++ controllers/productController.js
@@ -0,0 +1,76 @@
+const Product = require("../models/productsModel")
+
+// Get All Product
+const getProducts = async(req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products)
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Get Product By Id
+const getProduct = async(req, res) => {
+  try {
+    const {id} = req.params;
+    const products = await Product.findById(id);
+    res.status(200).json(products);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const createProduct = async(req, res) => {
+  try {
+    const product = await Product.create(req.body)
+    res.status(200).json(product)
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Update  Product
+const updatedProduct = async(req, res) => {
+  try {
+    const {id} = req.params;
+    const products = await Product.findByIdAndUpdate(id, req.body);
+
+    if(!products) {
+      return res.status(404).json({message: `Cannot find any product with ID ${id}`})
+    }
+
+    const updatedProduct = await Product.findById(id);
+    res.status(200).json(updatedProduct);
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Delete Product
+const deleteProduct = async(req, res) => {
+  try {
+      const {id} = req.params;
+      const products = await Product.findByIdAndDelete(id);
+
+      if(!products) {
+        return res.status(500).json({message: `Cannot find any product with ID ${id}`});
+      }
+      res.status(200).json(products);
+
+  } catch (error) {
+    res.status(500).json({message: error.message});
+  }
+};
+
+module.exports = {
+     getProducts,
+     getProduct,
+     createProduct,
+     updatedProduct,
+     deleteProduct
+}
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,73 +1,21 @@
 const express = require("express");
 const Product = require("../models/productsModel");
 const route = express.Router();
+const { getProducts, getProduct, createProduct, updatedProduct, deleteProduct } = require("../controllers/productController")
 
 // Edit Product
-route.put("/:id", async(req, res) => {
-  try {
-    const {id} = req.params;
-    const products = await Product.findByIdAndUpdate(id, req.body);
-
-    if(!products) {
-      return res.status(404).json({message: `Cannot find any product with ID ${id}`})
-    }
-
-    const updatedProduct = await Product.findById(id);
-    res.status(200).json(updatedProduct);
-
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
+route.put("/:id", updatedProduct);
 
 // Delete Product By ID
-route.delete("/:id", async(req, res) => {
-  try {
-      const {id} = req.params;
-      const products = await Product.findByIdAndDelete(id);
-
-      if(!products) {
-        return res.status(500).json({message: `Cannot find any product with ID ${id}`});
-      }
-      res.status(200).json(products);
-
-  } catch (error) {
-    res.status(500).json({message: error.message});
-  }
-});
+route.delete("/:id", deleteProduct);
 
 // Get Producy By ID
-route.get("/:id", async(req, res) => {
-  try {
-    const {id} = req.params;
-    const products = await Product.findById(id);
-    res.status(200).json(products);
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).json({ message: error.message });
-  }
-});
+route.get("/:id", getProduct);
 
 // Get All Products
-route.get("/", async(req, res) => {
-  try {
-    const products = await Product.find({});
-    res.status(200).json(products)
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).json({ message: error.message });
-  }
-});
+route.get("/", getProducts);
 
 // Post Products
-route.post("/", async(req, res) => {
-  try {
-    const product = await Product.create(req.body)
-    res.status(200).json(product)
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).json({ message: error.message });
-  }
-});
+route.post("/", createProduct);
 
 module.exports = route;
